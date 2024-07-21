5d0fa2ef500c37c8b1847cb9811af48cf3174696
Mujeeb02 <83205006+Mujeeb02@users.noreply.github.com>
2024-07-21 16:53:48 +0530

    server port fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 10000, // Use 3000 or the port you prefer
+    host: true, // Necessary to make the server accessible externally
+  },
 })
